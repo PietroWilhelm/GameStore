662cb18d80ac04740e500563eef7aeeb014324b1
PietroWilhelm <pietrowilhelm@git.com>
2026-03-23 09:27:39 -0300

    feature: Adicionado os pacotes Entity e Oracle no projeto, além da preparação para conexeção do banco

--- GameStore/GameStore.Domain/obj/Debug/net10.0/GameStore.Domain.AssemblyInfo.cs
+++ GameStore/GameStore.Domain/obj/Debug/net10.0/GameStore.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2b08ff3f0c7796fb53c1f9900d1692be1a4d36a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dab8b626c9187bb87da41bd5939cb1caf688554")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ GameStore/GameStore.Infrastructure/Persistence/GameStoreContext.cs
@@ -0,0 +1,22 @@
+﻿using GameStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Infrastructure.Persistence;
+
+public class GameStoreContext : DbContext
+{
+    public GameStoreContext(DbContextOptions<GameStoreContext> GameStoreOptions) : base(GameStoreOptions)
+    {
+        
+    }
+    
+    public DbSet<Game> Games { get; set; }
+
+    public DbSet<Genre> Genres { get; set; }
+    
+    public DbSet<Customer> customers { get; set; }
+    
+    public DbSet<Order> Orders { get; set; }
+    
+    public DbSet<Studio> Studios { get; set; }
+}
