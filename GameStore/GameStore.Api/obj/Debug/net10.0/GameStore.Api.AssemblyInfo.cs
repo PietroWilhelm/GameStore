3d597c7de6d04676e7b51c84aba45ac5bfad3a46
PietroWilhelm <pietrowilhelm@git.com>
2026-04-13 10:30:11 -0300

    feat: adicionado Microsoft.EntityFrameworkCore.Tools

--- GameStore/GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3516a601f4e32f7cc27d906908e79f76fc08e32e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4efaf8234d7cda6a24fe00314954c5966fe9e6bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
